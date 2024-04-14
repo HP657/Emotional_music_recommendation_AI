2ea0e4b0d9dcfa2d6e88d31a2d2dba32c390b58a
HP657 <junlee5070@gmail.com>
2024-04-14 23:58:35 +0900

    일단 static에서 html/index.html으로 정적으로 불러올수있게만 해둠 css javascript 연결은 아직

--- feelow/src/main/java/com/example/feelow/controller/mainController.java
+++ feelow/src/main/java/com/example/feelow/controller/mainController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class controller {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String mainsite() {
-        
+        return "main";
     }
 }
