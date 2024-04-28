f08460f8e6ea1f795d4d4236a1c4d4b479827eec
HP657 <junlee5070@gmail.com>
2024-04-28 18:40:51 +0900

    로그 추가

--- feelow/src/main/java/com/example/feelow/api/MainApiController.java
+++ feelow/src/main/java/com/example/feelow/api/MainApiController.java
@@ -3,11 +3,12 @@ package com.example.feelow.api;
 import com.example.feelow.dto.EmotionRequest;
 import com.example.feelow.dto.EmotionResponse;
 import com.example.feelow.service.EmotionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class MainApiController {
 
@@ -16,8 +17,11 @@ public class MainApiController {
 
     @PostMapping("/api/recommend")
     public EmotionResponse recommend(@RequestBody EmotionRequest request) {
+        log.debug("Request received: {}", request);
         String emotion = request.getEmotion();
+        log.info("Processing emotion: {}", emotion);
         String recommendation = emotionService.processEmotion(emotion);
+        log.info("Generated recommendation: {}", recommendation);
         return new EmotionResponse(recommendation);
     }
 }
