b17b92d52db9e341086c4d98a892363916a28aab
HP657 <junlee5070@gmail.com>
2024-06-02 20:32:04 +0900

    감정이 아닌 문장이 들어오도록함

--- feelow/src/main/java/com/example/feelow/api/MainApiController.java
+++ feelow/src/main/java/com/example/feelow/api/MainApiController.java
@@ -3,25 +3,20 @@ package com.example.feelow.api;
 import com.example.feelow.dto.EmotionRequest;
 import com.example.feelow.dto.EmotionResponse;
 import com.example.feelow.service.EmotionService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-@Slf4j
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+@RequestMapping("/api")
 public class MainApiController {
 
     @Autowired
     private EmotionService emotionService;
 
-    @PostMapping("/api/recommend")
+    @PostMapping("/recommend")
     public EmotionResponse recommend(@RequestBody EmotionRequest request) {
-        log.debug("Request received: {}", request);
-        String emotion = request.getEmotion();
-        log.info("Processing emotion: {}", emotion);
-        String recommendation = emotionService.processEmotion(emotion);
-        log.info("Generated recommendation: {}", recommendation);
+        String sentence = request.getSentence();
+        String recommendation = emotionService.processEmotion(sentence);
         return new EmotionResponse(recommendation);
     }
 }
